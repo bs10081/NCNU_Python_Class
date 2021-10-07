73bd57fea185c8300b648997ccb5181f64fe08a0
Cx330 <38148679+bs10081@users.noreply.github.com>
2021-10-07 16:52:53 +0800

    Finish the python class homework and the class example

--- Week2/findPrime/findPrime.py
+++ Week2/findPrime/findPrime.py
@@ -1,28 +1,26 @@
-# 學號：110213027
-# 姓名：簡齊君
-v = None    # v = None, Global function.
+v = None                                                # v = None, Global function.
 
 def main():
     printPrimes(isInput())
 
 def isInput():
-    global v    # Use Global variavle "v"
-    Begin = int(input("Begin Number: "))
-    End = int(input("End Number: "))
-    v = min(Begin, End)     # Get the min Number to fix Order problem.  
-    n = max(Begin, End)     # Get the max Number to fix Order problem.
+    global v                                            # Use Global variavle "v"
+    Begin = int(input())
+    End = int(input())
+    v = min(Begin, End)                                 # Get the min Number to fix Order problem.5
+    n = max(Begin, End)                                 # Get the max Number to fix Order problem.
     return n
-def printPrimes(n):     # Define a function to print primes.
+def printPrimes(n):                                     # Define a function to print primes.
     
     global v
-    # print v from 1 to n.
+                                                        # print v from 1 to n.
     while v <= n:
         if isPrime(v):
             print(v)
         v = v + 1
 
-def isPrime(v):     # Check number is prime.
-    # Divide v by one until it can’t be divide.
+def isPrime(v):                                         # Check number is prime.
+                                                        # Divide v by one until it can’t be divide.
     div = 2 
     while div < v:
         if v % div == 0:
