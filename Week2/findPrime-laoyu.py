9a1ca0fb41b399c4223214d093216dddf2ac71b5
Cx330 <38148679+bs10081@users.noreply.github.com>
2021-10-08 17:35:17 +0800

    Add Class program.

---
+++ Week2/findPrime-laoyu.py
@@ -0,0 +1,22 @@
+def main():
+    v = int(input())
+    n = int(input())
+    if v > n:
+        n,v = v,n
+        
+    while v <= n:
+        if isPrime(v):
+            print(v)
+        v = v + 1
+
+
+
+def isPrime(v):                                      
+    div = 2 
+    while div*div < v:
+        if v % div == 0:
+            return False
+        div = div + 1
+    return True
+
+main()
