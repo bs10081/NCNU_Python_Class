105f50b60cae8a995cce648217832c13d080b625
RegChien <bs10081@bs10081.com>
2021-10-14 10:38:14 +0000

    Only keep the content of LaoYu's class.

--- Week2/findPrime/findPrime.py
+++ Week2/findPrime/findPrime.py
@@ -1,32 +1,22 @@
-v = None    # v = None, Global function.
-
 def main():
-    printPrimes(isInput())
-
-def isInput():
-    
-    global v                                            # Use Global variavle "v"
-
-    Begin = int(input())
-    End = int(input())
-    v = min(Begin, End)                                 # Get the min Number to fix Order problem.5
-    n = max(Begin, End)                                 # Get the max Number to fix Order problem.
-    return n
-def printPrimes(n):                                     # Define a function to print primes.
-    
-    global v
-                                                        # print v from 1 to n.
+    v = int(input())
+    n = int(input())
+    if v > n:
+        n,v = v,n
+        
     while v <= n:
         if isPrime(v):
             print(v)
         v = v + 1
 
-def isPrime(v):                                         # Check number is prime.
-                                                        # Divide v by one until it can’t be divide.
+
+
+def isPrime(v):                                      
     div = 2 
-    while div < v:
+    while div*div < v:
         if v % div == 0:
             return False
         div = div + 1
     return True
+
 main()
