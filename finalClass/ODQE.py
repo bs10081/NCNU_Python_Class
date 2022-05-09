035ad9b590b45f51c08431a16249e7c2cd362b25
bs10081 <bs10081@bs10081.com>
2022-05-09 08:44:04 +0800

    Creat brench

--- finalClass/ODQE.py
+++
@@ -1,8 +0,0 @@
-def main():
-    a = float(input())
-    b = float(input())
-    c = float(input())
-
-    print((-b + (b*b - 4*a*c)**0.5) / (a*2), (-b - (b*b - 4*a*c)**0.5) / (a*2))
-if __name__ == "__main__":
-    main()
