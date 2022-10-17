08c182f15a9129384475cc038db23d87c230fd46
bs10081 <bs10081@bs10081.com>
2022-10-17 15:16:50 +0800

    [Del]刪除之前的內容

--- sum.py
+++
@@ -1,9 +0,0 @@
-a = [1, 2, 3, 4]
-sumNum = 0
-
-for i in a:
-    # In the for loop, "i" will sequentially read the values in list a.
-    sumNum += i 
-    # sumNum = sumNum + i, means, 0 + 1 > (0+1) + 2 > (1+2) + 3 > (3+3) + 4
-
-print(sumNum)
